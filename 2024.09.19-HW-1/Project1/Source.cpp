f45b7a5bf68fcd423195bd65d9ada1622227ee9c
st128884 <st128884@student.spbu.ru>
2024-12-04 17:14:15 +0300

    HW-1 all tasks upload

---
+++ 2024.09.19-HW-1/Project1/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+
+	scanf_s("%d", &b);
+	scanf_s("%d", &a);
+
+	printf("%d\n", a);
+	printf("%d", b);
+
+	return EXIT_SUCCESS;
+}
